01e337deae441cd1de7bebbdfbdd3debb00fa9cd
Tjitske Lohstroh <tjitske@lohstroh.nl>
2016-12-17 00:32:53 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214195757) do
+ActiveRecord::Schema.define(version: 20161216220137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "category"
+    t.decimal  "price"
+    t.string   "img_url"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "smoothies", force: :cascade do |t|
     t.string   "name"
