dfd45a599430bec9abd43987580ea618b7a4100d
Tjitske Lohstroh <tjitske@lohstroh.nl>
2016-12-17 00:31:56 +0100

    Add ingredients to home page

--- app/controllers/smoothies_controller.rb
+++ app/controllers/smoothies_controller.rb
@@ -1,5 +1,9 @@
 class SmoothiesController < ApplicationController
   def index
     @smoothies = Smoothie.all
+    @ingredients = Ingredient.all
+    @liquids = @ingredients.liquids
+    @solids = @ingredients.solids
+    @spices = @ingredients.spices
   end
 end
